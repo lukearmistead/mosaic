0e793cc7c45fde1cc11b95a9bf488a085c9b1ad5
Luke Armistead <luke.armistead@omadahealth.com>
2021-01-17 14:50:32 -0800

    Integrating new data extraction flow with final dashboard

--- arete/app.py
+++ arete/app.py
@@ -1,3 +1,4 @@
+import logging
 import streamlit as st
 from os import listdir
 from os.path import isfile, join
@@ -8,10 +9,19 @@ import matplotlib.dates as mdates
 import datetime
 import altair as alt
 
-PATH = '../data/fitbit/'
+PATH = 'data/fitbit/'
 heart_files = [f for f in listdir(PATH) if isfile(join(PATH, f)) and 'heart' in f]
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
 
 # Get DataFrame
+logging.info('Loading data from CSVs')
 dfs = []
 for heart in heart_files:
  dfs.append(pd.read_csv(PATH + heart))
@@ -20,6 +30,7 @@ df['dateTime'] = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in df['d
 
 
 # Dashboard
+logging.info('Outlining Streamlit dashboard parameters')
 st.title('Fitness Dashboard')
 
 c = alt.Chart(df).mark_line().encode(
@@ -34,8 +45,10 @@ fig = plt.figure()
 ax1 = plt.subplot(2,1,1)
 ax2 = plt.subplot(2,1,2, sharex=ax1)
 dateform = DateFormatter('%B')
-ax1.plot(df['dateTime'],df['restingHeartRate'])
-ax2.plot(df['dateTime'],df['Fat Burn'])
+
+logging.info('Defining axes')
+ax1.plot(df['dateTime'], df['restingHeartRate'])
+ax2.plot(df['dateTime'], df['Fat Burn'])
 ax1.xaxis.set_major_formatter(dateform)
 ax1.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 
