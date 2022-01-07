b5f2e5b71cc0bc3a9ea7d643c7e0546a07415d35
Luke Armistead <luke.armistead@omadahealth.com>
2022-01-06 20:37:26 -0800

    Completing platform to extract data from strava

--- arete/extract_strava.py
+++ arete/extract_strava.py
@@ -1,11 +1,7 @@
-from datetime import datetime, timedelta
-from io import StringIO
 import logging
 import pandas as pd
-import stravalib
-import requests
 from stravaio import strava_oauth2, StravaIO
-from utils import yaml_lookup
+from utils import lookup_yaml, update_yaml
 
 
 logging.basicConfig(
@@ -18,30 +14,45 @@ logging.basicConfig(
 
 CREDS_KEY = 'strava'
 CREDS_PATH = 'creds.yml'
-OUTPUT_DIR_PATH='data/strava/'
-
-def main(creds_path, creds_key):
-
-    # Get creds
-    creds = yaml_lookup(creds_path, creds_key)
-    client_id, client_secret = creds['client_id'], creds['client_secret']
-    print('found client')
+SEASON_STARTS_ON = '2021-10-01'
+DATA_OUTPUT_PATH ='data/strava/activities.csv'
 
 
-    output = strava_oauth2(client_id, client_secret)
-    print('made connection')
-    access_token = output['access_token']
-    client = StravaIO(access_token)
-    print('made connection 2')
+class Creds:
+    def __init__(self, creds_path, creds_key):
+        creds = lookup_yaml(creds_path)[creds_key]
+        self.client_id = creds['client_id']
+        self.client_secret = creds['client_secret']
+        self.access_token = creds['access_token']
+        if not self.is_connected():
+            refresh_access_token()
+            update_yaml(creds_path, {creds_key: {'access_token': self.access_token}})
 
-    activities = client.get_logged_in_athlete_activities(after='2021-12-01')
-    print(len(activities))
-    print(activities[0])
-    print(activities[1])
-    print(activities[2])
-    print(activities)
+    def refresh_access_token(self):
+        output = strava_oauth2(self.client_id, self.client_secret)
+        self.access_token = output['access_token']
+
+    def is_connected(self):
+        client = StravaIO(self.access_token)
+        if client.get_logged_in_athlete():
+            return True
+        else:
+            logging.info('Access token failed to establish connection to Strava')
+            return False
+
+
+def extract_to_dataframe(raw_data) -> pd.DataFrame:
+    processed_data = [entry.to_dict() for entry in raw_data]
+    return pd.DataFrame(processed_data)
+
+
+def main(extract_data_since, creds_path, creds_key, data_output_path):
+    creds = Creds(creds_path, creds_key)
+    client = StravaIO(creds.access_token)
+    raw_data = client.get_logged_in_athlete_activities(after=extract_data_since)
+    df = extract_to_dataframe(raw_data)
+    df.to_csv(data_output_path)
 
 
 if __name__ == '__main__':
-    print('Works!')
-    main(CREDS_PATH, CREDS_KEY)
+    main(SEASON_STARTS_ON, CREDS_PATH, CREDS_KEY, DATA_OUTPUT_PATH)
