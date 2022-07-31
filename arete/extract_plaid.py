02baf53c666c197fce3b5663eceaf02631797bd0
Luke Armistead <luke.armistead@omadahealth.com>
2022-07-31 10:45:45 -0700

    Getting a basic extract running with Plaid

--- arete/extract_plaid.py
+++ arete/extract_plaid.py
@@ -6,40 +6,55 @@ import pandas as pd
 import plaid
 from plaid.api import plaid_api
 from plaid.model.transactions_get_request import TransactionsGetRequest
-from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
-from utils import Creds, lookup_yaml
+from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
+from plaid.model.item_public_token_exchange_request import (
+    ItemPublicTokenExchangeRequest,
+)
+from ratelimit import limits, sleep_and_retry
+from utils import lookup_yaml
 
 
 log.getLogger().setLevel(log.INFO)
 CREDS_KEY = "plaid"
 ACCESS_TOKEN_KEY = "plaid_account"
 CREDS_PATH = "creds.yml"
-END_DATE = datetime.now().date()
-START_DATE = END_DATE - timedelta(days=5)
-ACCOUNTS = ["aspiration", "barclays", "chase", "capitalone"]
-OUTPUT_DIR_PATH = "../data/plaid/"
+
+END_DATE = datetime.now().date()  #  - timedelta(days=360)
+START_DATE = END_DATE - timedelta(days=360)
+
+# Some institution-specific limitations:
+# Plaid's agreement with Capital One only permits downloading the last 90 days of transactions
+# https://dashboard.plaid.com/oauth-guide
+# Barclaycard is actually a distinct entity from Barclays. Plaid doesn't enable access to the former.
+ACCOUNTS = ["aspiration", "chase", "capital_one"]
+OUTPUT_DIR_PATH = "data/plaid/"
 
 
-def get_transaction_data(client, access_token, start_date, end_date) -> pd.DataFrame:
-    """Gets around Plaid's rate limit"""
-    running_date = end_date
-    dfs = []
-    dumb = TransactionsGetRequest(access_token=access_token, start_date=start_date, end_date=running_date)
-    while running_date > start_date:
-        d = client.transactions_get(dumb)
-        df = pd.DataFrame(d["transactions"])
-        earliest_date = df["date"].min()
-        # Check that we aren't stuck because of data gaps
-        if running_date == earliest_date:
-            break
-        df = df.loc[
-            df["date"] > earliest_date,
-        ]  # Drop the stub
-        dfs.append(df)
-        running_date = earliest_date
-    df = pd.concat(dfs)
-    log.info(f"DataFrame created:\n{df.info()}")
-    return df
+def get_client(creds):
+    configuration = plaid.Configuration(
+        host=plaid.Environment.Development,
+        api_key={"clientId": creds["client_id"], "secret": creds["client_secret"],},
+    )
+    api_client = plaid.ApiClient(configuration)
+    client = plaid_api.PlaidApi(api_client)
+    return client
+
+
+@sleep_and_retry
+@limits(calls=30, period=60)
+def fetch_transactions_response(client, access_token, start_date, end_date, options):
+    request = TransactionsGetRequest(
+        access_token=access_token,
+        start_date=start_date,
+        end_date=end_date,
+        options=options,
+    )
+    try:
+        response = client.transactions_get(request)
+    except:
+        print("Hit rate limit unexpectedly. Sleeping a full minute to reset.")
+        time.sleep(60)
+    return response.to_dict()
 
 
 def main(
@@ -50,66 +65,50 @@ def main(
     accounts=ACCOUNTS,
     output_dir_path=OUTPUT_DIR_PATH,
 ):
-
     creds = lookup_yaml(creds_path)[creds_key]
-    '''
-    Debug
-    Are the client id and secret alright? 
-    > Yes.
+    client = get_client(creds)
+    for account in accounts:
+        access_token = creds[account]["access_token"]
+        # Transactions in the response are paginated, so make multiple calls
+        # while increasing the offset to retrieve all transactions
+        options = TransactionsGetRequestOptions()
+        transactions = []
+        has_more_transactions = True
+        while has_more_transactions:
+            options.offset = len(transactions)
+            response = fetch_transactions_response(
+                client, access_token, start_date, end_date, options
+            )
+            transactions += response["transactions"]
+            # We create the boolean coordinating the loop after receiving the expected transaction count from the plaid response
+            has_more_transactions = len(transactions) < response["total_transactions"]
 
-    Did I call the configuration properly?
-    > Pretty sure I did, yes.
-
-    What does stack overflow say about this?
-    > https://stackoverflow.com/questions/42098126/mac-osx-python-ssl-sslerror-ssl-certificate-verify-failed-certificate-verify
-
-    Do I have an SSL certificate?
-    > Seemingly yes.
-
-    Is there something weird about Omada's security settings?
-    > TBD
-
-    If none of the above work, let's ask Plaid Support.
-    '''
-
-    configuration = plaid.Configuration(
-        host=plaid.Environment.Development,
-        api_key={
-            'clientId': creds['client_id'],
-            'secret': creds['client_secret'],
+        # Build dataframe
+        df = pd.DataFrame(transactions)
+        column_types = {
+            "transaction_id": "object",
+            "datetime": "datetime64[ns]",
+            "date": "datetime64[ns]",
+            "account_id": "object",
+            "category_id": "int64",
+            "category": "object",
+            "merchant_name": "object",
+            "amount": "float64",
         }
-    )
-    api_client = plaid.ApiClient(configuration)
-    client = plaid_api.PlaidApi(api_client)
+        df = df.astype(dtype=column_types)[column_types.keys()]
 
-    # Can I do anything with Plaid?
-
-
-    # Is it the capital one account?
-    access_token = creds['aspiration']['access_token']
-
-    request = TransactionsGetRequest(access_token=access_token, start_date=start_date, end_date=end_date)
-    print(request)
-    response = client.transactions_get(request)
-    print('ERROR CODE')
-    print(response['error_code'])
-    print('got response')
-   
-    transactions = response['transactions']
-    print(transactions)
-    # for account in accounts:
-    #     log.info(
-    #         f"Pulling transactions from {account} account from {start_date} to {end_date}"
-    #     )
-    #     access_token = lookup_yaml(CREDS_PATH)[creds_key][account]["access_token"]
-    #     print(access_token)
-    #     df = get_transaction_data(client, access_token, start_date, end_date)
-    #     if output_dir_path:
-    #         name = account + "_transactions"
-    #         name = "{}_{}_to_{}.csv".format(name, end_date, start_date)
-    #         df.to_csv(output_dir_path + name, index=False)
-    #     if not output_dir_path:
-    #         return dfs
+        # Outputs for debugging
+        log.info(
+            f"{account} transaction extract for {start_date} till {end_date} complete"
+        )
+        # log.info(f"""{account} earliest and latest transaction dates: {df["date"].min()}, {df["date"].max()}""")
+        log.info(df.head())
+        log.info(df.info())
+        log.info(
+            f"Returned {len(df)} row table, vs expectation of {response['total_transactions']}"
+        )
+        log.info(response["total_transactions"])
+        df.to_csv(OUTPUT_DIR_PATH + account + "_transactions.csv", index=False)
 
 
 if __name__ == "__main__":
